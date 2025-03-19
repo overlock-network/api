fd88469871975a592a6abf637b3292c5433c39d2
Evgheni Poleacov <evgheni.poleacov@gmail.com>
2025-03-19 15:23:49 +0200

    moved protobuf template files

--- script/generate.sh
+++ script/generate.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-CURRENT_DIR=$(pwd)
-
-cd proto/node || exit 1
-
 EXCLUDE_PATHS=$(find . -type f -name "module.proto" | sed 's/^/--exclude-path=/' | tr '\n' ' ')
 
 buf generate \
@@ -17,12 +13,13 @@ buf generate \
   --log-format=json \
   --template=buf.gen.pulsar.yaml
 
+rm -p ../../.cache/swagger
+
 buf generate \
   --error-format=json \
   --log-format=json \
-  --template=buf.gen.swagger.yaml
-
-cd "$CURRENT_DIR" || exit 1
+  --template=buf.gen.swagger.yaml \
+  --output ../../.cache/swagger
 
 cp -rv go/github.com/web-seven/overlock-api/. ./
 rm -rf go/github.com
