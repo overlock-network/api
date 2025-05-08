01ebb37948613e22f37d08944e027f7d47f9b4bd
Evgheni Poleacov <evgheni.poleacov@gmail.com>
2025-05-08 16:52:05 +0300

    generated Go structs

--- ts/src/overlock/crossplane/v1beta1/environment.ts
+++ ts/src/overlock/crossplane/v1beta1/environment.ts
@@ -9,6 +9,8 @@ export interface Environment {
   id: bigint;
   /** creator */
   creator: string;
+  /** provider */
+  provider: bigint;
 }
 export interface EnvironmentProtoMsg {
   typeUrl: "/overlock.crossplane.v1beta1.Environment";
@@ -22,6 +24,8 @@ export interface EnvironmentAmino {
   id?: string;
   /** creator */
   creator?: string;
+  /** provider */
+  provider?: string;
 }
 export interface EnvironmentAminoMsg {
   type: "/overlock.crossplane.v1beta1.Environment";
@@ -32,12 +36,14 @@ export interface EnvironmentSDKType {
   metadata?: MetadataSDKType;
   id: bigint;
   creator: string;
+  provider: bigint;
 }
 function createBaseEnvironment(): Environment {
   return {
     metadata: undefined,
     id: BigInt(0),
-    creator: ""
+    creator: "",
+    provider: BigInt(0)
   };
 }
 export const Environment = {
@@ -51,6 +57,9 @@ export const Environment = {
     }
     if (message.creator !== "") {
       writer.uint32(26).string(message.creator);
+    }
+    if (message.provider !== BigInt(0)) {
+      writer.uint32(32).uint64(message.provider);
     }
     return writer;
   },
@@ -70,6 +79,9 @@ export const Environment = {
         case 3:
           message.creator = reader.string();
           break;
+        case 4:
+          message.provider = reader.uint64();
+          break;
         default:
           reader.skipType(tag & 7);
           break;
@@ -82,6 +94,7 @@ export const Environment = {
     message.metadata = object.metadata !== undefined && object.metadata !== null ? Metadata.fromPartial(object.metadata) : undefined;
     message.id = object.id !== undefined && object.id !== null ? BigInt(object.id.toString()) : BigInt(0);
     message.creator = object.creator ?? "";
+    message.provider = object.provider !== undefined && object.provider !== null ? BigInt(object.provider.toString()) : BigInt(0);
     return message;
   },
   fromAmino(object: EnvironmentAmino): Environment {
@@ -95,6 +108,9 @@ export const Environment = {
     if (object.creator !== undefined && object.creator !== null) {
       message.creator = object.creator;
     }
+    if (object.provider !== undefined && object.provider !== null) {
+      message.provider = BigInt(object.provider);
+    }
     return message;
   },
   toAmino(message: Environment): EnvironmentAmino {
@@ -102,6 +118,7 @@ export const Environment = {
     obj.metadata = message.metadata ? Metadata.toAmino(message.metadata) : undefined;
     obj.id = message.id !== BigInt(0) ? message.id?.toString() : undefined;
     obj.creator = message.creator === "" ? undefined : message.creator;
+    obj.provider = message.provider !== BigInt(0) ? message.provider?.toString() : undefined;
     return obj;
   },
   fromAminoMsg(object: EnvironmentAminoMsg): Environment {
